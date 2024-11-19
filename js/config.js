db048889f8e5c48fa1af5a0dd42d396062090fef
01andres <98999112+01andres@users.noreply.github.com>
2024-11-19 17:59:30 -0500

    adicionde local host en config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
 
 //export const URL_API = process.env.URL_API || 'http://www.saludanimalcitas2024.somee.com/api';
 
-export const URL_API = import.meta.env.VITE_API_URL;
+export const URL_API = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
 
 
 
