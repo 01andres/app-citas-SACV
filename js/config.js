737038452eeb2543ffdd81f8fca460fe78c323b0
01andres <98999112+01andres@users.noreply.github.com>
2024-11-19 17:53:03 -0500

    cambiando la variable de entorno con VITE_

--- js/config.js
+++ js/config.js
@@ -1,6 +1,9 @@
 //export const URL_API = 'http://www.saludanimalcitas2024.somee.com/api';
 
-export const URL_API = process.env.URL_API || 'http://www.saludanimalcitas2024.somee.com/api';
+//export const URL_API = process.env.URL_API || 'http://www.saludanimalcitas2024.somee.com/api';
+
+export const URL_API = import.meta.env.VITE_API_URL;
+
 
 
 //
