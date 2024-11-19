4600730723dc16c6d5b43341cb74ee157d9484d4
01andres <98999112+01andres@users.noreply.github.com>
2024-11-19 18:15:23 -0500

    retornando a la variable de entorno original

--- js/config.js
+++ js/config.js
@@ -1,12 +1,7 @@
-//export const URL_API = 'http://www.saludanimalcitas2024.somee.com/api';
+export const URL_API = 'http://www.saludanimalcitas2024.somee.com/api';
 
 //export const URL_API = process.env.URL_API || 'http://www.saludanimalcitas2024.somee.com/api';
 
-const DEFAULT_API_URL = 'http://localhost:3000/api'; // Fallback para desarrollo local
-
-export const URL_API = typeof import.meta.env !== 'undefined' && import.meta.env.VITE_API_URL
-  ? import.meta.env.VITE_API_URL
-  : DEFAULT_API_URL;
 
 
 
