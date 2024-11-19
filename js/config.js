976372fbaff05563ae0e15d85bba29943b4c741f
01andres <98999112+01andres@users.noreply.github.com>
2024-11-19 17:39:16 -0500

    cambio 2 varible de entorno

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 //export const URL_API = 'http://www.saludanimalcitas2024.somee.com/api';
 
-export const URL_API = process.env.API_URL;
+export const URL_API = process.env.URL_API || 'http://www.saludanimalcitas2024.somee.com/api';
 
 
 //
