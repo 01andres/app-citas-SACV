1fc544b5714c6e6d37e6fb29e4e825cea345895e
01andres <98999112+01andres@users.noreply.github.com>
2024-11-19 18:07:32 -0500

    definir valor por defecto de VITE

--- js/config.js
+++ js/config.js
@@ -2,7 +2,11 @@
 
 //export const URL_API = process.env.URL_API || 'http://www.saludanimalcitas2024.somee.com/api';
 
-export const URL_API = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+const DEFAULT_API_URL = 'http://localhost:3000/api'; // Fallback para desarrollo local
+
+export const URL_API = typeof import.meta.env !== 'undefined' && import.meta.env.VITE_API_URL
+  ? import.meta.env.VITE_API_URL
+  : DEFAULT_API_URL;
 
 
 
